fb92d417c12abbfb2118f5e48d7e74b9602efa85
kl0nee <tarky.gc@gmail.com>
2022-08-24 17:13:34 -0300

    little update

--- commands/aeval.js
+++ commands/aeval.js
@@ -1,51 +1,51 @@
 module.exports = {
-	name: "aeval",
-	aliases: ["ae"],
-	category: "desarrolladores",
-	owners: true,
-	desc: "Prueba un codigo",
-	usage: "<prefix>aeval <code>",
-	execute: async(client, message, args) =>{
-	const Discord = require("discord.js")
-	try {
-    if(!args.join(" ")) return message.channel.send("<a:deny:718249526217015338> | Debes poner algo para evaluar")
-	const code = await eval("(async() => { "+args.join(" ")+" })()")
-	const type = typeof code
-	const types = {
-		_configs1: {
-		string: "Texto",
-		number: "Numero",
-		object: "Objeto",
-		"function": "Función",
-		array: "Array",
-		promise: "Promesa",
-    boolean: "Booleano",
-		},
-		err: "Error:"
-	}
-	      if (typeof code !== "string")
-        code = require("util").inspect(code, { depth: 0 });
-	const embed = new Discord.MessageEmbed()
-	.setDescription("<a:approve:734933612906020974> | Evaluado en `"+client.ws.ping+"` ms")
-	.addField("Entrada:", "```js\n"+args.join(" ")+"\n```")
-	.addField("Salida:", "```js\n"+code.replace(client.token, process.env.TOKEN_FOUNDED_IN_A_GLITCH_PROYECTXD)+"\n```")
-	.addField("Tipo:", "```js\n"+types._configs[type]+"\n```")
-	.setColor(client.color)
-	message.channel.send(embed)
-	} catch(e) {
-	    const types = {
-		/*_configs: {
-		string: "Texto",
-		number: "Numero",
-		object: "Objeto",
-		"function": "Función",
-		array: "Array",
-		promise: "Promesa",
-    boolean: "Booleano",
-		},*/
-		err: "Error:"
-	}
-	message.channel.send("`ERROR`: ```js\n"+types.err+e.message+"\n```")
-	}
-	}
-}
+  name: 'aeval',
+  aliases: ['ae'],
+  category: 'desarrolladores',
+  owners: true,
+  desc: 'Prueba un codigo',
+  usage: '<prefix>aeval <code>',
+  execute: async (client, message, args) => {
+    const Discord = require('discord.js');
+    try {
+      if (!args.join(' '))
+        return message.channel.send(
+          '<a:deny:718249526217015338> | Debes poner algo para evaluar'
+        );
+      const code = await eval('(async() => { ' + args.join(' ') + ' })()');
+      const type = typeof code;
+      if (typeof code !== 'string')
+        code = require('util').inspect(code, { depth: 0 });
+      const embed = {
+        description: '<a:approve:734933612906020974> | Evaluado en `0` ms',
+        color: 0xe29a1c,
+        timestamp: new Date(),
+        fields: [
+          {
+            name: 'Entrada:',
+            value: '```js\n' + args.join(' ') + '\n```',
+            inline: false,
+          },
+          {
+            name: 'Salida:',
+            value:
+              '```js\n' +
+              code.replace(client.token, process.env.GLITCH_TOKEN) +
+              '\n```',
+            inline: false,
+          },
+          {
+            name: 'Tipo:',
+            value: '```js\n' + types._configs[type] + '\n```',
+            inline: false,
+          },
+        ],
+      };
+      message.channel.send(embed);
+    } catch (e) {
+      message.channel.send(
+        '`ERROR`: ```js\n' + types.err + e.message + '\n```'
+      );
+    }
+  },
+};
--- index.js
+++ index.js
@@ -1,72 +1,19 @@
 const eris = require('eris');
-// Structures
-//let { Structures } = require("discord.js")
 const { currentPrefix, setPrefix } =
   new (require('./utils/functions/prefix.js'))();
-/*
-little update
-// Guild structure
-Structures.extend("Guild", Guild =>{
-return class extends Guild{
- constructor(client, data) {
- super(client, data);
- this.prefix = {
- current: currentPrefix(client, this.id),
- change: async(newprefix) =>{ 
- if(!newprefix) return 'puto';
- return setPrefix(client, this.id, newprefix)},
- has: currentPrefix(client, this.id) == "n." ? false : true
- }
- this.options = {
- prefix: currentPrefix(client, this.id) == "n." ? false : true
- }
- }
-}
-})
-// User structure
-Structures.extend("User", (User) =>{
-return class extends User{
-constructor(client, data) {
- super(client, data);
- this.options = {
-developer: client.devs.includes(data.id) ? true : false,
-cool: true
-}
-}
-}
-})
-// GuildMember structure
-Structures.extend("GuildMember", (GuildMember) =>{
-return class extends GuildMember{
-constructor(client, data, guild) {
-super(client, data, guild);
-this.options = {
-developer: client.devs.includes(data.id) ? true : false,
-superCool: true
-}
-}
- hasPermission(permission, { checkAdmin = true, checkOwner = true } = {}) {
- if(checkOwner && this.user.id === this.guild.ownerID) return true;
- if(permission == "NAIKO_DEVELOPER") return client.devs.includes(this.user.id) ? true : false 
- return this.roles.cache.some(r => r.permissions.has(permission, checkAdmin));
- }
-}
-})
-//Message structure
-Structures.extend("Message", (Message) =>{
-return class extends Message{
-constructor(client, data, channel) {
-super(client, data, channel);
-this.server = this.guild
-}
-}
-})*/
-//let BaseClient = require("./utils/structures/Client.js")
-const client = new BaseClient('Bot ' + process.env['TOKEN']);
+const client = new BaseClient("Bot "+ process.env.TOKEN, {
+        options: {
+           autoreconnect: true,
+           allowedMentions: {
+              everyone: true,
+              roles: true,
+              users: true
+           },
+           maxShards: "auto"
+        }
+     }));
 const fs = require('fs');
-//Connecting to database
 const mongoose = require('./utils/mongoose.js');
-//Functions
 client.queue = new Discord.Collection();
 client.antiinvite = require('./utils/models/prefixes.js');
 client.prefixes = require('./utils/models/prefixes.js');
@@ -85,15 +32,12 @@ client.cooldown = new Discord.Collection();
 client.comandos = new Discord.Collection();
 client.eventos = new Discord.Collection();
 client.editsnipes = new Map();
-//Handlers
-//Commands
 for (const cmdName of fs
   .readdirSync('./commands')
   .filter((k) => k.endsWith('.js'))) {
   const comando = require('./commands/' + cmdName);
   client.comandos.set(comando.name, comando);
 }
-//Events
 for (const evntName of fs
   .readdirSync('./events')
   .filter((k) => k.endsWith('.js'))) {
@@ -101,4 +45,4 @@ for (const evntName of fs
   client.on(evntName.replace('.js', ''), evento.bind(null, client));
   client.eventos.set(evntName.replace('.js', ''), evento);
 }
-client.login(process.env.TOKEN);
+client.connect(process.env.TOKEN);
